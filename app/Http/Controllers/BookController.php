ed25e61dece73e2ccfda19852a577f617cd863ea
kuvinci <kuvinci@gmail.com>
2023-11-19 17:22:46 +0200

    Adding Book and Tag controllers

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+
+    public function create() {
+
+    }
+
+    public function remove() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function destroy() {
+
+    }
+
+}
---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+
+    public function create() {
+
+    }
+
+    public function remove() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function assign() {
+
+    }
+
+}
