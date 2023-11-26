6bebf86750285c7e95aa9b1a52d7630ddaf977a4
kuvinci <kuvinci@gmail.com>
2023-11-26 23:16:45 +0200

    Adding BookList->remove() method to delete books

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -9,7 +9,23 @@ class BookList extends Component
 {
     public $books;
 
+    public function remove($bookId)
+    {
+        if($bookId){
+            Book::destroy($bookId);
+
+            // Reset books list
+            $this->fetchData();
+            $this->dispatch('flash-message', ['message' => 'Book successfully deleted.']);
+        }
+    }
+
     public function mount()
+    {
+        $this->fetchData();
+    }
+
+    public function fetchData()
     {
         $this->books = Book::with('tags')->get();
     }
